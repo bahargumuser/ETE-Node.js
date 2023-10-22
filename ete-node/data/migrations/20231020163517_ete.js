35c1f6bedf461842ddd033021e8ab751ce7a0446
Bahar Gümüşer <121288876+bahargumuser@users.noreply.github.com>
2023-10-22 18:29:08 +0300

    migrate finish

---
+++ ete-node/data/migrations/20231020163517_ete.js
@@ -0,0 +1,37 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return( knex.schema 
+    .createTable("Companies" , (tablo) => {
+        tablo.increments("companies_id");
+        tablo.string("company_name").notNullable();
+        tablo.integer("company_legal_number");
+        tablo.string("incorporation_country");
+        table.string("website_url");
+    })
+    .createTable("Products", (tablo) => {
+        tablo.increments("product_id");
+        tablo.string("product_name").notNullable();
+        tablo.string("product_category"); 
+        tablo.integer("product_amount");
+        tablo.integer("product_unit");
+        tablo.integer("companies_id").notNullable().references("companies_id").inTable("Companies").onUpdate("CASCADE").onDelete("CASCADE");
+    }).createTable("Users" , (tablo) => {
+        tablo.increments("user_id");
+        tablo.string("fullname");
+        tablo.string("email").notNullable().unique();
+        tablo.string("password").notNullable();
+    })
+    )
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists("Users").dropTableIfExists("Products").dropSchemaIfExists("Companies");
+  
+};
